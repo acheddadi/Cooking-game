8e737c7038297f4c7135e3eedc30a38f7021d08c
Tyler <tyler@programmer.net>
2020-03-07 13:37:20 -0500

    Timer & Customer Setup

---
+++ Assets/Scripts/GlobalTimer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalTimer : MonoBehaviour
+{
+
+    public float startingTime = 60;
+    public float currentTime = 0;
+
+    public Text timerText;
+
+    private void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = currentTime.ToString("0");
+
+        if (currentTime > 10f)
+        {
+            timerText.color = Color.white;
+        }
+        else {
+            timerText.color = Color.red;
+
+            if (currentTime <= 0f)
+            {
+                // Game Over
+                Debug.Log("Game Over");
+            }
+        }
+
+    }
+
+    public void AddTime(float time)
+    {
+        currentTime += time;
+    }
+
+    public void RemoveTime(float time)
+    {
+        currentTime -= time;
+    }
+}
