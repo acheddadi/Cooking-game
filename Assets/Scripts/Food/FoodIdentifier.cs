0b2ec5a4885b24e62cd5cf6007dd6b8494e4314a
acheddadi <alicheddadi@gmail.com>
2020-03-08 16:06:53 -0400

    Food behaviours implemented.

---
+++ Assets/Scripts/Food/FoodIdentifier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TypeOfFood { SUSHI, SASHIMI, YAKITORI, TEMPURA, EDAMAME, EMPTY }
+public class FoodIdentifier : MonoBehaviour
+{
+    [SerializeField] private TypeOfFood foodType = TypeOfFood.EMPTY;
+
+    public TypeOfFood GetFoodType()
+    {
+        return foodType;
+    }
+}
---
+++ Assets/Scripts/Robot/FoodGathering.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodGathering : MonoBehaviour
+{
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private InventoryController inventory;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) GatherFood();
+    }
+
+    private void GatherFood()
+    {
+        Debug.Log("GatherFood");
+        RaycastHit hitInfo;
+        if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hitInfo, Mathf.Infinity))
+        {
+            FoodIdentifier foodToAdd = hitInfo.collider.GetComponent<FoodIdentifier>();
+            if (foodToAdd != null)
+            {
+                Debug.Log("Hit food.");
+                if (inventory.AddFood(foodToAdd.GetFoodType())) Destroy(foodToAdd.gameObject);
+                else Debug.Log("Inventory is full!");
+            }
+        }
+    }
+}
